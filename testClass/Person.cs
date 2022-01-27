ab204be0f9ef38dd712615833aa7c7314cf2dad6
Admin <Admin@DESKTOP-0ARTQQ5>
2022-01-27 12:42:46 +0300

    Добавьте файлы проекта.

---
+++ testClass/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testClass
+{
+    class Person
+    {
+        private string _name;
+        private string _familyName;
+        private int _age;
+
+        public Person() 
+        {
+            
+        }
+
+        public Person(string name)
+        {
+            _name = name;
+        }
+
+        public void SetName(string name) 
+        {
+            _name = name;                      
+        }
+
+        public string GetName() 
+        {
+            return _name;
+        }
+
+    }
+}
---
+++ testClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace testClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person("Vova");
+            person1.SetName("Dmytro");
+            Console.WriteLine(person1.GetName()); 
+        }
+    }
+}
+     
+     
+     
+     
+     
+     
